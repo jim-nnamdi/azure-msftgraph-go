ec9277176797199e92790929c93f9f075c1ac788
jim-nnamdi <jimsamuel50@gmail.com>
2022-08-14 23:10:46 +0100

    (pkg) -> interface initialization and azure model structs

--- pkg/azure.go
+++ pkg/azure.go
@@ -1,1 +1,8 @@
 package azure
+
+import "context"
+
+type Client interface {
+	AzureCreateNewUser(ctx context.Context, email, password, firstname, lastname string)
+	AzureAddExtensionToUser(ctx context.Context, sessionkey string)
+}
--- pkg/azure_msftgraph.go
+++ pkg/azure_msftgraph.go
@@ -1,1 +1,26 @@
 package azure
+
+import "context"
+
+var _ Client = &azuremodel{}
+
+type azuremodel struct {
+	host          string
+	clientID      string
+	clientSecret  string
+	loginClientID string
+}
+
+func NewAzureModel(host string, clientID string, clientSecret string, loginClientID string) *azuremodel {
+	return &azuremodel{
+		host:          host,
+		clientID:      clientID,
+		clientSecret:  clientSecret,
+		loginClientID: loginClientID,
+	}
+}
+
+func (model *azuremodel) AzureCreateNewUser(ctx context.Context, email, password, firstname, lastname string) {
+}
+
+func (model *azuremodel) AzureAddExtensionToUser(ctx context.Context, sessionkey string) {}
